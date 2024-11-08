38338c50e3141cf028489c04de9280923fc56d7d
adudu21isme <74877817+adudu21isme@users.noreply.github.com>
2024-11-08 23:52:59 +0000

    Create default.bat

---
+++ default.bat
@@ -0,0 +1,24 @@
+@echo off
+title Anonymized Studio
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Erase Credentials? y/n:
+IF /I "%AREYOUSURE%" NEQ "y" GOTO GO
+for /f "tokens=1,2 delims=" %%a in ('cmdkey /list ^| find "www.roblox.com"') do ( cmdkey /delete:%%b > nul 2>&1)
+echo Erased Credentials
+:GO
+endlocal
+echo Erasing Cache and Roblox AppData...
+powershell -Command "Remove-Item '%userprofile%\AppData\LocalLow\rbxcsettings.rbx' -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\AnalysticsSettings.xml' -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\LocalStorage' -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\logs' -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\ScriptAutosaves' -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\UserCaptures' -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\Sandbox' -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\pluginIDEState' -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Roblox\InstalledPlugins' -Recurse -Force -ErrorAction SilentlyContinue; Remove-Item '%userprofile%\AppData\Local\Temp' -Recurse -Force -ErrorAction SilentlyContinue"
+echo Adjusting Studio Registry...
+reg add "HKCU\Software\Roblox\RobloxStudio\LoggedInUsersStore\https:\www.roblox.com" /v "users" /t REG_SZ /d "{}" /f > nul 2>&1
+reg add "HKCU\Software\ROBLOX Corporation\Environments\roblox-studio\Capabilities" /v "ApplicationName" /t REG_SZ /d "BAD ROBLOX STUDIO" /f > nul 2>&1
+reg add "HKCU\Software\ROBLOX Corporation\Environments\roblox-studio\Capabilities" /v "ApplicationDescription" /t REG_SZ /d "Please use Roblox Studio Internal" /f > nul 2>&1
+reg delete "HKCU\Software\Roblox\RobloxStudio\RobloxStudioFirstTimeLoggedIn" /f > nul 2>&1
+reg delete "HKCU\Software\Roblox\RobloxStudio\RobloxStudioMostRecentLogin" /f > nul 2>&1
+reg delete "HKCU\Software\Roblox\RobloxStudio\RobloxStudioLaunchTrackingGuid" /f > nul 2>&1
+reg delete "HKCU\Software\Roblox\RobloxStudio\DirtyPublishWarningDialog" /f > nul 2>&1
+echo Checking Studio Registry...
+powershell -Command "$robloxStudioPath='Registry::HKEY_CURRENT_USER\Software\Roblox\RobloxStudio';$subKeys=Get-ChildItem -Path $robloxStudioPath;$regexPattern='^(rbxRecentRobloxApiGames_v02_\d+|\d+(_\d+)?|(\d+rbxRecentFiles_v03))$';$foundKeys=@();foreach($key in $subKeys){if($key.PSChildName -match $regexPattern){$foundKeys+= $robloxStudioPath + '\' + $key.PSChildName;}}if($foundKeys.Count -gt 0){Write-Host '';$foundKeys | ForEach-Object {Write-Host $_};$confirmation=Read-Host ' ';if($confirmation -eq 'yes'){foreach($keyPath in $foundKeys){Remove-Item -Path $keyPath -Recurse -Force -ErrorAction SilentlyContinue}Write-Host 'Deleted UserIds';}}"
+echo Launching...
+start "" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Roblox\Roblox Studio.lnk"
